d60f8930954048b67af9b8da423c11a4774f115a
Вадим Волков <vdm.volkov249@mail.ru>
2021-06-19 05:40:14 +0400

    4

--- XamarinTest/RegistratePage.xaml.cs
+++ XamarinTest/RegistratePage.xaml.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using XamarinTest.models;
 
 namespace XamarinTest
 {
@@ -17,10 +21,19 @@ namespace XamarinTest
             InitializeComponent();
         }
 
-        private void btn_Regist(object sender, System.EventArgs e)
+        private async void btn_Regist(object sender, System.EventArgs e)
         {
-            Button button = (Button)sender;
-            button.Text = "Нажато!";
+            /*Button button = (Button)sender;
+            button.Text = "Нажато!";*/
+            Patient p = new Patient(
+                txt_Snils.Text,txt_Name.Text,txt_Surname.Text,txt_Patronymic.Text,new DateTime().Date,Double.Parse(txt_Height.Text), Double.Parse(txt_Weight.Text)
+                );
+            string jsonPatient = JsonSerializer.Serialize(p);
+            HttpClient client = Client.GetClient();
+            var response = await client.PostAsync(Client.Url+ "/addPatient",
+                new StringContent(
+                    jsonPatient,
+                    Encoding.UTF8, "application/json"));
         }
     }
 }
---
+++ XamarinTest/models/Patient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinTest.models
+{
+    class Patient
+    {
+        //класс клиента
+        public string Snils { get; set; } //снилс
+        public string Name { get; set; } //имя
+        public string Surname { get; set; } //фамилия
+        public string Patronymic { get; set; } //отчество
+        public DateTime DateOfBirthDay { get; set; } // возраст
+        public double Height { get; set; }// рост
+        public double Weight { get; set; }//вес
+
+        public Patient(string snils, string name, string surname, string patronymic, DateTime dateOfBirthDay, double height, double weight)
+        {
+            Snils = snils;
+            Name = name;
+            Surname = surname;
+            Patronymic = patronymic;
+            DateOfBirthDay = dateOfBirthDay;
+            Height = height;
+            Weight = weight;
+        }
+    }
+}
