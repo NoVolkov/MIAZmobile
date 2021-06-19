c06a892f32374aab75873dd1aec3d9c71e992c0a
Вадим Волков <vdm.volkov249@mail.ru>
2021-06-19 06:24:44 +0400

    Revert "Revert "4""
    This reverts commit 2400c30df7b55c8d4de13caf3b636eb49a472428.

---
+++ WebApplication1/models/Patient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.models
+{
+    public class Patient
+    {
+        //класс клиента
+        public int Id { get; set; }
+        public string Snils { get; set; } //снилс
+        public string Name { get; set; } //имя
+        public string Surname { get; set; } //фамилия
+        public string Patronymic { get; set; } //отчество
+        public DateTime DateOfBirthDay { get; set; } // возраст
+        public double Height { get; set; }// рост
+        public double Weight { get; set; }//вес
+    }
+}
---
+++ XamarinTest/models/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace XamarinTest.models
+{
+    class Client
+    {
+        public static string Url = "https://localhost:44350/";
+        public static HttpClient GetClient()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            return client;
+        }
+    }
+}
