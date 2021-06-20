56bcc82fe034a7c542831e9afadc4b56641ff321
Вадим Волков <vdm.volkov249@mail.ru>
2021-06-20 10:46:00 +0400

    Revert "31"
    This reverts commit 45632a4c3dc48a10d8d6b74ab2528e7046795b30.

--- XamarinTest/RegistratePage.xaml.cs
+++ XamarinTest/RegistratePage.xaml.cs
@@ -25,8 +25,18 @@ namespace XamarinTest
         //
         private async void btn_Regist(object sender, System.EventArgs e)
         {
-            
-            await Navigation.PushModalAsync(new AddPage());
+            /*Button button = (Button)sender;
+            button.Text = "Нажато!";*/
+            /* Patient p = new Patient(
+                 txt_Snils.Text,txt_Name.Text,txt_Surname.Text,txt_Patronymic.Text,new DateTime().Date,Double.Parse(txt_Height.Text), Double.Parse(txt_Weight.Text)
+                 );
+             string jsonPatient = JsonSerializer.Serialize(p);
+             HttpClient client = Client.GetClient();
+             var response = await client.PostAsync(Client.Url+ "/addPatient",
+                 new StringContent(
+                     jsonPatient,
+                     Encoding.UTF8, "application/json")); */
+            await Navigation.PushAsync(new AddPage());
         }
     }
 }
